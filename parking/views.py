30fe306754b08333a5752bd95f9927a6dd0a40e0
northoutLokesh <lokesh.pathak@northout.com>
2020-02-23 01:22:46 +0530

    Creted view to display list of parked vehicles

--- parking/views.py
+++ parking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .models import Vehicle, Slot, Parking
+from .serializers import ParkingSerializer, SlotSerializer, VehicleSerializer
 # Create your views here.
+
+
+class ParkingSerializerListView(generics.ListAPIView):
+    queryset = Parking.objects.all()
+    serializer_class = ParkingSerializer
+    pagination_class = None
