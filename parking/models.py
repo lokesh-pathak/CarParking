c79dc5556a3015a140d8a74a4abcc634cfdb1fe6
northoutLokesh <lokesh.pathak@northout.com>
2020-02-22 19:11:15 +0530

    added model for vehicle, slot and parking

--- parking/models.py
+++ parking/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class Vehicle(models.Model):
+    registration_number = models.CharField(max_length=20, blank=True, null=True)
+    colour = models.CharField(max_length=20, blank=True, null=True)
+
+    class Meta:
+        db_table = 'vehicle'
+
+
+class Slot(models.Model):
+    position = models.IntegerField(blank=True, null =True)
+    status = models.CharField(max_length=12, choices=settings.STATUSES, default=settings.STATUSES[0][0])
+
+    class Meta:
+        db_table = 'slot'
+
+
+class Parking(models.Model):
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    slot = models.ForeignKey(Slot)
+
+    class Meta:
+        db_table = 'parking'
